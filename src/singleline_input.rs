58064de471bc8c25333b8bb9a5e83806a98bcd8f
ddbaker <donarno.dan.baker@gmail.com>
2025-11-25 23:16:47 +0900

    rename main.rs to src/singleline_input.rs

--- src/singleline_input.rs
+++ src/singleline_input.rs
@@ -3,7 +3,6 @@ use gpui_component::{
     input::{Input, InputEvent, InputState},
     *,
 };
-use gpui_component_assets::Assets;
 
 pub struct SingleLineInput {
     sl_input_state: Entity<InputState>,
@@ -17,7 +16,7 @@ pub struct SingleLineInput {
 }
 
 impl SingleLineInput {
-    fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
+    pub fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
         let sl_input_state = cx.new(|cx| InputState::new(window, cx).placeholder("Enter your name"));
 
         let _sl_subscriptions = vec![cx.subscribe_in(&sl_input_state, window, {
@@ -51,29 +50,4 @@ impl Render for SingleLineInput {
             .child(Input::new(&self.sl_input_state))
             .child(self.display_text.clone())
     }
-}
-
-fn main() {
-    let app = Application::new().with_assets(Assets);
-
-    app.run(move |cx| {
-        // This must be called before using any GPUI Component features.
-        gpui_component::init(cx);
-
-        let window_options = WindowOptions {
-            window_bounds: Some(WindowBounds::centered(size(px(800.), px(600.)), cx)),
-            ..Default::default()
-        };
-
-        cx.spawn(async move |cx| {
-            cx.open_window(window_options, |window, cx| {
-                let view = cx.new(|cx| SingleLineInput::new(window, cx));
-                // This first level on the window, should be a Root.
-                cx.new(|cx| Root::new(view, window, cx))
-            })?;
-
-            Ok::<_, anyhow::Error>(())
-        })
-        .detach();
-    });
 }
