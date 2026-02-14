5219ad4839bbccb0dcfa79332bdfcd1e2f402887
ddbaker <donarno.dan.baker@gmail.com>
2026-02-15 02:04:40 +0900

    editor is integrated.
    new file:   src/main.rs
    modified:   src/singleline_input.rs

--- src/singleline_input.rs
+++ src/singleline_input.rs
@@ -1,33 +1,25 @@
 use gpui::*;
-use gpui_component::{
-    input::{Input, InputEvent, InputState},
-    *,
-};
+use gpui_component::input::{Input, InputEvent, InputState};
 
 pub struct SingleLineInput {
     sl_input_state: Entity<InputState>,
     display_text: SharedString,
-
-    /// We need to keep the subscriptions alive with the SingleLineInput entity.
-    ///
-    /// So if the SingleLineInput entity is dropped, the subscriptions are also dropped.
-    /// This is important to avoid memory leaks.
     _sl_subscriptions: Vec<Subscription>,
 }
 
 impl SingleLineInput {
     pub fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
-        let sl_input_state = cx.new(|cx| InputState::new(window, cx).placeholder("Enter your name"));
+        let sl_input_state =
+            cx.new(|cx| InputState::new(window, cx).placeholder("Type here..."));
 
         let _sl_subscriptions = vec![cx.subscribe_in(&sl_input_state, window, {
             let sl_input_state = sl_input_state.clone();
-            move |this, _, ev: &InputEvent, _window, cx| match ev {
-                InputEvent::Change => {
+            move |this, _, ev: &InputEvent, _window, cx| {
+                if let InputEvent::Change = ev {
                     let value = sl_input_state.read(cx).value();
                     this.display_text = format!("Hello, {}!", value).into();
-                    cx.notify()
+                    cx.notify();
                 }
-                _ => {}
             }
         })];
 
@@ -41,13 +33,6 @@ impl SingleLineInput {
 
 impl Render for SingleLineInput {
     fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
-        v_flex()
-            .p_5()
-            .gap_2()
-            .size_full()
-            .items_center()
-            //.justify_center()
-            .child(Input::new(&self.sl_input_state))
-            //.child(self.display_text.clone())
+        Input::new(&self.sl_input_state).w_full()
     }
 }
